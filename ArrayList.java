3374713d088f734e73d3f0ca38db5db85da7a25c
Ankush-ITA <127494609+Ankush-ITA@users.noreply.github.com>
2023-06-10 15:45:42 +0530

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -5,7 +5,8 @@ public static void main(String[] args)
     {
   
         // create an empty array list with an initial capacity
-        ArrayList<Integer> arrlist = new ArrayList<Integer>(5);
+        ArrayList<Integer> arrlist = new ArrayList<Integer>(5); 
+  //
   
         // use add() method to add elements in the list
         arrlist.add(15);
